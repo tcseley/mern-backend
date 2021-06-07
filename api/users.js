7fb3dac9b7e385682f9db2cf5f59424536d6d74c
tcseley <tiborcseley@gmail.com>
2021-06-07 09:26:54 -0500

    user api/model connection

--- api/users.js
+++ api/users.js
@@ -8,6 +8,7 @@ const passport = require('passport');
 const JWT_SECRET = process.env.JWT_SECRET;
 
 // Models
+const { User } = require('../models');
 
 // controllers
 const test = async (req, res) => {
